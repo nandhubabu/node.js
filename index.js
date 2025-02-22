513761fbe36fc2209e5ff7d9809c9bedef0e1d6c
Nandhu Babu <nandhubabuvktd@gmail.com>
2025-02-23 00:39:41 +0530

    Fix console log formatting for silly name output

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 var genaratename = require('sillyname')
 var sillyname = genaratename()
-console.log('Your silly name is: ' + sillyname)
-// module.exports = sillyname
+console.log('Your silly name is: ' + sillyname);
